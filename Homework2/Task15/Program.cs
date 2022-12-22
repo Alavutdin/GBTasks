fb8d79cbb30dc1431e9d159b22fc24c7a1b48547
atlan <alavutdin_atlan@mail.ru>
2022-12-22 17:02:53 +0300

    Add Task13

---
+++ Homework2/Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Enter a number");
+int number = int.Parse(ReadLine()!);
+
+if (number > 7) {   WriteLine("Вы вышли за пределы недели.");}
+else 
+if (number == 1){   WriteLine("Нет");}
+if (number == 2){   WriteLine("Нет");}
+if (number == 3){   WriteLine("Нет");}
+if (number == 4){   WriteLine("Нет");}
+if (number == 5){   WriteLine("Нет");}
+if (number == 6){   WriteLine("Да");}
+if (number == 7){   WriteLine("Да");}
