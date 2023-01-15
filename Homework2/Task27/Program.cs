1d1a5bfbef69acd038860a444133d5bde9089bc7
atlan <alavutdin_atlan@mail.ru>
2023-01-15 18:33:21 +0300

    Add Task27

---
+++ Homework2/Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 27: Напишите программу, реализующую метод, 
+который принимает на вход число и 
+выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12*/
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+WriteLine($"{GetSum(number)}");
+
+
+int GetSum (int A) // 4 type вид метода принимает и возвращает return
+{
+    int result = 0;
+    while (A>0)
+    {
+        result = result+A%10;
+        A/=10;       
+    }
+    
+    return result;
+}
